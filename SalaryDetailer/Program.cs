8c94b4bbf67b9eca4a9e0987abcca1c9beb7b9e9
Tony Nguyen <tony.m.n@gmail.com>
2017-12-06 19:56:01 +1100

    More refactoring. Error handling for user input. Clean up of Salary class.

--- SalaryDetailer/Program.cs
+++ SalaryDetailer/Program.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Globalization;
+using System.IO;
 
 namespace SalaryDetailer
 {
@@ -12,53 +13,65 @@ namespace SalaryDetailer
     {
         static void Main(string[] args)
         {
-            Console.Write("Enter your salary package amount: ");
-            String input = Console.ReadLine();
-
             /*
-             * Number group separators (commas) are acceptable. 
-             * If copied and pasted from a source with commas, replace with nothing.
-             */
-            input.Replace(CultureInfo.CurrentUICulture.NumberFormat.NumberGroupSeparator, "");
+                * Read user input for salary package amount. 
+                * Strip known valid characters.
+                * try parse; whilst false
+                */
+
+            Console.Write("Enter your salary package amount: ");
+            string input = Console.ReadLine();
+            input = input.Replace(CultureInfo.CurrentUICulture.NumberFormat.NumberGroupSeparator, "");
+            input = input.Replace(CultureInfo.CurrentUICulture.NumberFormat.CurrencySymbol, "");
 
             decimal grossSalary;
-            if (decimal.TryParse(input, out grossSalary))
+            while (!decimal.TryParse(input, out grossSalary))
             {
-                char payFreq = 'W';
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("Please enter a valid salary package amount.\n");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.Write("Enter your salary package amount: ");
+                input = Console.ReadLine();
+                input = input.Replace(CultureInfo.CurrentUICulture.NumberFormat.NumberGroupSeparator, "");
+                input = input.Replace(CultureInfo.CurrentUICulture.NumberFormat.CurrencySymbol, "");
+            }
 
+            /*
+            * Read user input for pay frequency. 
+            * Massage the input, grab the first character.
+            * Validate input; whilst false
+            */
+            Console.Write("Enter your pay frequency (W for weekly, F for fortnightly, M for monthly): ");
+            char payFreq = Console.ReadLine().ToUpper()[0];
+            while (payFreq != 'W' && payFreq != 'F' && payFreq != 'M')
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.Write("Please enter a valid pay frequency.\n");
+                Console.ForegroundColor = ConsoleColor.Gray;
                 Console.Write("Enter your pay frequency (W for weekly, F for fortnightly, M for monthly): ");
                 payFreq = Console.ReadLine().ToUpper()[0];
-                while (payFreq != 'W' && payFreq != 'F' && payFreq != 'M')
-                {
-                    Console.Write("Enter your pay frequency (W for weekly, F for fortnightly, M for monthly): ");
-                    payFreq = Console.ReadLine().ToUpper()[0];
-                }
-
-                // get super percentage from App Settings
-                decimal superannuationPercentage;
-                decimal.TryParse(ConfigurationManager.AppSettings["superannuationPercentage"], out superannuationPercentage);
-
-                Salary Salary = new Salary(grossSalary, superannuationPercentage, payFreq);
-
-                Console.WriteLine("\nCalculating salary details...\n");
-                Console.WriteLine("Gross package: " + Salary.GrossSalary.ToString("C", CultureInfo.CurrentCulture));
-                Console.WriteLine("Superannuation: " + Salary.Superannuation.ToString("C", CultureInfo.CurrentCulture));
-                Console.WriteLine("\nTaxable income: " + Salary.TaxableIncome.ToString("C", CultureInfo.CurrentCulture));
-
-                Console.WriteLine("\nDeductions:");
-                Console.WriteLine("Medicare Levy: " + Salary.MedicareLevy.ToString("C", CultureInfo.CurrentCulture));
-                Console.WriteLine("Budget Repair Levy: " + Salary.BudgetRepairLevy.ToString("C", CultureInfo.CurrentCulture));
-
-                Console.WriteLine("Income Tax: " + Salary.IncomeTax.ToString("C", CultureInfo.CurrentCulture));
-
-                Console.WriteLine("\nNet income: " + Salary.NetIncome.ToString("C", CultureInfo.CurrentCulture));
-
-                Console.WriteLine("Pay packet: " + Salary.PayPacket.ToString("C", CultureInfo.CurrentCulture));
             }
-            else
-            {
-                Console.WriteLine("The entered amount is not a valid salary value.");
-            }
+            /*
+            * Get super percentage from App Settings
+            * Instantiate new 
+            */
+            decimal.TryParse(ConfigurationManager.AppSettings["superannuationPercentage"], out decimal superannuationPercentage);
+            Salary Salary = new Salary(grossSalary, superannuationPercentage, payFreq);
+
+            Console.WriteLine("\nCalculating salary details...\n");
+            Console.WriteLine("Gross package: " + Salary.GrossSalary.ToString("C", CultureInfo.CurrentCulture));
+            Console.WriteLine("Superannuation: " + Salary.Superannuation.ToString("C", CultureInfo.CurrentCulture));
+            Console.WriteLine("\nTaxable income: " + Salary.TaxableIncome.ToString("C", CultureInfo.CurrentCulture));
+
+            Console.WriteLine("\nDeductions:");
+            Console.WriteLine("Medicare Levy: " + Salary.MedicareLevy.ToString("C", CultureInfo.CurrentCulture));
+            Console.WriteLine("Budget Repair Levy: " + Salary.BudgetRepairLevy.ToString("C", CultureInfo.CurrentCulture));
+
+            Console.WriteLine("Income Tax: " + Salary.IncomeTax.ToString("C", CultureInfo.CurrentCulture));
+
+            Console.WriteLine("\nNet income: " + Salary.NetIncome.ToString("C", CultureInfo.CurrentCulture));
+
+            Console.WriteLine("Pay packet: " + Salary.PayPacket);
 
             Console.Write("\nPress any key to end...");
             Console.ReadKey();
--- SalaryDetailer/Salary.cs
+++ SalaryDetailer/Salary.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,9 @@ namespace SalaryDetailer
     {
         private decimal _grossSalary;
         public decimal GrossSalary { get { return _grossSalary; } set { _grossSalary = value; } }
+
+        private decimal _superannuationPercentage;
+        public decimal SuperannuationPercentage { get { return _superannuationPercentage; } set { _superannuationPercentage = value; } }
 
         private char _payFrequency;
         public char PayFrequency { get { return _payFrequency; } set { _payFrequency = value; } }
@@ -32,14 +36,23 @@ namespace SalaryDetailer
         private decimal _netIncome;
         public decimal NetIncome { get { return _netIncome; } set { _netIncome = value; } }
 
-        private decimal _payPacket;
-        public decimal PayPacket { get { return _payPacket; } set { _payPacket = value; } }
+        private string _payPacket;
+        public string PayPacket { get { return _payPacket; } set { _payPacket = value; } }
+
+        /*
+         * TODO Create a data type to store the taxable income threshold & a string mathematical expression
+         * e.g.
+         * 21335,0
+         * 26668,{TI}-(21335*0.1)
+         */
 
         public Salary(decimal grossSalary, decimal superannuationPercentage, char payFrequency)
         {
-            GrossSalary = grossSalary;
-            PayFrequency = payFrequency; 
-            CalculateTaxableIncome(superannuationPercentage);
+            _grossSalary = grossSalary;
+            _payFrequency = payFrequency;
+            _superannuationPercentage = superannuationPercentage;
+
+            CalculateTaxableIncome();
             CalculateSuperannuation();
             CalculateMedicareLevy();
             CalculateBudgetRepairLevy();
@@ -48,74 +61,75 @@ namespace SalaryDetailer
             CalculatePayPacket();
         }
 
-        private void CalculateTaxableIncome(decimal superannuationPercentage)
+        private void CalculateTaxableIncome()
         {
-            TaxableIncome = GrossSalary / ((100 + superannuationPercentage) / 100);
+            _taxableIncome = _grossSalary / ((100 + _superannuationPercentage) / 100);
         }
 
         private void CalculateSuperannuation()
         {
-            Superannuation = GrossSalary - TaxableIncome;
+            _superannuation = _grossSalary - _taxableIncome;
         }
 
         private void CalculateMedicareLevy()
         {
             decimal medicareLevy = 0;
-            if (TaxableIncome > 21335 && TaxableIncome <= 26668)
-                medicareLevy = (TaxableIncome - 21335) * (decimal)0.1;
-            else if (TaxableIncome > 26669)
-                medicareLevy = TaxableIncome * (decimal)0.02;
+            if (_taxableIncome > 21335 && _taxableIncome <= 26668)
+                medicareLevy = (_taxableIncome - 21335) * (decimal)0.1;
+            else if (_taxableIncome > 26669)
+                medicareLevy = _taxableIncome * (decimal)0.02;
 
-            MedicareLevy = medicareLevy;
+            _medicareLevy = decimal.Ceiling(medicareLevy);
         }
 
         private void CalculateBudgetRepairLevy()
         {
             decimal budgetRepairLevy = 0;
-            if (TaxableIncome > 180000)
-                budgetRepairLevy = (TaxableIncome - 180000) * (decimal)0.02;
+            if (_taxableIncome > 180000)
+                budgetRepairLevy = (_taxableIncome - 180000) * (decimal)0.02;
 
-            BudgetRepairLevy = budgetRepairLevy;
+            _budgetRepairLevy = decimal.Ceiling(budgetRepairLevy);
         }
 
         private void CalculateIncomeTax()
         {
             decimal incomeTax = 0;
-            if (TaxableIncome > 18200 && TaxableIncome <= 37000)
-                incomeTax = (TaxableIncome - 18200) * (decimal)0.19;
-            else if (TaxableIncome > 37000 && TaxableIncome <= 87000)
-                incomeTax = 3572 + ((TaxableIncome - 37000) * (decimal)0.325);
-            else if (TaxableIncome > 87000 && TaxableIncome <= 180000)
-                incomeTax = 19822 + ((TaxableIncome - 87000) * (decimal)0.37);
-            else if (TaxableIncome > 180001)
-                incomeTax = 54232 + ((TaxableIncome - 180000) * (decimal)0.47);
+            if (_taxableIncome > 18200 && _taxableIncome <= 37000)
+                incomeTax = (_taxableIncome - 18200) * (decimal)0.19;
+            else if (_taxableIncome > 37000 && _taxableIncome <= 87000)
+                incomeTax = 3572 + ((_taxableIncome - 37000) * (decimal)0.325);
+            else if (_taxableIncome > 87000 && _taxableIncome <= 180000)
+                incomeTax = 19822 + ((_taxableIncome - 87000) * (decimal)0.37);
+            else if (_taxableIncome > 180001)
+                incomeTax = 54232 + ((_taxableIncome - 180000) * (decimal)0.47);
 
-            IncomeTax = incomeTax;
+            _incomeTax = decimal.Ceiling(incomeTax);
         }
 
         private void CalculateNetIncome()
         {
-            decimal netIncome = GrossSalary - Superannuation - MedicareLevy - BudgetRepairLevy - IncomeTax;
-            NetIncome = netIncome;
+            decimal netIncome = _grossSalary - _superannuation - _medicareLevy - _budgetRepairLevy - _incomeTax;
+            _netIncome = netIncome;
         }
 
         private void CalculatePayPacket()
         {
-            decimal payPacket = 0;
+            string payPacket;
             switch (PayFrequency)
             {
                 case 'W':
-                    payPacket = (NetIncome / 365) * 7;
+                    payPacket = ((decimal)((NetIncome / 365) * 7)).ToString("C", CultureInfo.CurrentCulture) + " per week";
                     break;
                 case 'F':
-                    payPacket = (NetIncome / 365) * 14;
+                    payPacket = ((decimal)((NetIncome / 365) * 14)).ToString("C", CultureInfo.CurrentCulture) + " per fortnight";
                     break;
                 case 'M':
-                    payPacket = NetIncome / 12;
+                default:
+                    payPacket = ((decimal)(NetIncome / 12)).ToString("C", CultureInfo.CurrentCulture) + " per month";
                     break;
             }
 
-            PayPacket = payPacket;
+            _payPacket = payPacket;
         }
     }
 }
