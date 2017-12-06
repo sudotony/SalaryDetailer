d053ac8bbb89fb07e2e22c458277429af71ad973
Tony Nguyen <tony.m.n@gmail.com>
2017-12-06 17:20:45 +1100

    Refactored. Added Salary class.

--- SalaryDetailer/Program.cs
+++ SalaryDetailer/Program.cs
@@ -16,89 +16,47 @@ namespace SalaryDetailer
             String input = Console.ReadLine();
 
             /*
-             * Basic string manipulation. If copied and pasted from a source with commas, replace with nothing.
-             * Commas are acceptable.
+             * Number group separators (commas) are acceptable. 
+             * If copied and pasted from a source with commas, replace with nothing.
              */
-            input.Replace(",", "");
+            input.Replace(CultureInfo.CurrentUICulture.NumberFormat.NumberGroupSeparator, "");
 
-            /*
-             * decimal probably most appropriate type for grossSalary
-             */
-            float grossSalary;
-            if (float.TryParse(input, out grossSalary))
+            decimal grossSalary;
+            if (decimal.TryParse(input, out grossSalary))
             {
-                //good
+                char payFreq = 'W';
+
                 Console.Write("Enter your pay frequency (W for weekly, F for fortnightly, M for monthly): ");
-                String payFreq = Console.ReadLine();
-                // TODO parse the pay frequency 
+                payFreq = Console.ReadLine().ToUpper()[0];
+                while (payFreq != 'W' && payFreq != 'F' && payFreq != 'M')
+                {
+                    Console.Write("Enter your pay frequency (W for weekly, F for fortnightly, M for monthly): ");
+                    payFreq = Console.ReadLine().ToUpper()[0];
+                }
 
                 // get super percentage from App Settings
-                float superannuationPercentage;
-                float.TryParse(ConfigurationManager.AppSettings["superannuationPercentage"], out superannuationPercentage);
+                decimal superannuationPercentage;
+                decimal.TryParse(ConfigurationManager.AppSettings["superannuationPercentage"], out superannuationPercentage);
 
-                // calculate Taxable income
-                float taxableIncome = grossSalary / ((100 + superannuationPercentage) / 100);
-                float superannuation = grossSalary - taxableIncome;
+                Salary Salary = new Salary(grossSalary, superannuationPercentage, payFreq);
 
                 Console.WriteLine("\nCalculating salary details...\n");
-                Console.WriteLine("Gross package: " + grossSalary.ToString("C", CultureInfo.CurrentCulture));
-                Console.WriteLine("Superannuation: " + superannuation.ToString("C", CultureInfo.CurrentCulture));
-                Console.WriteLine("\nTaxable income: " + taxableIncome.ToString("C", CultureInfo.CurrentCulture));
+                Console.WriteLine("Gross package: " + Salary.GrossSalary.ToString("C", CultureInfo.CurrentCulture));
+                Console.WriteLine("Superannuation: " + Salary.Superannuation.ToString("C", CultureInfo.CurrentCulture));
+                Console.WriteLine("\nTaxable income: " + Salary.TaxableIncome.ToString("C", CultureInfo.CurrentCulture));
 
                 Console.WriteLine("\nDeductions:");
+                Console.WriteLine("Medicare Levy: " + Salary.MedicareLevy.ToString("C", CultureInfo.CurrentCulture));
+                Console.WriteLine("Budget Repair Levy: " + Salary.BudgetRepairLevy.ToString("C", CultureInfo.CurrentCulture));
 
-                // calculate Medicare Levy
-                float medicareLevy = 0;
-                if (taxableIncome > 21335 && taxableIncome <= 26668)
-                    medicareLevy = (taxableIncome - 21335) * (float)0.1;
-                else if (taxableIncome > 26669)
-                    medicareLevy = taxableIncome * (float)0.02;
-                Console.WriteLine("Medicare Levy: " + medicareLevy.ToString("C", CultureInfo.CurrentCulture));
+                Console.WriteLine("Income Tax: " + Salary.IncomeTax.ToString("C", CultureInfo.CurrentCulture));
 
-                // calculate Budget Repair Levy
-                float budgetRepairLevy = 0;
-                if (taxableIncome > 180000)
-                    budgetRepairLevy = (taxableIncome - 180000) * (float)0.02;
-                Console.WriteLine("Budget Repair Levy: " + budgetRepairLevy.ToString("C", CultureInfo.CurrentCulture));
+                Console.WriteLine("\nNet income: " + Salary.NetIncome.ToString("C", CultureInfo.CurrentCulture));
 
-                // calculate Income Tax
-                float incomeTax = 0;
-                if (taxableIncome > 18200 && taxableIncome <= 37000)
-                    incomeTax = (taxableIncome - 18200) * (float)0.19;
-                else if (taxableIncome > 37000 && taxableIncome <= 87000)
-                    incomeTax = 3572 + ((taxableIncome - 37000) * (float)0.325);
-                else if (taxableIncome > 87000 && taxableIncome <= 180000)
-                    incomeTax = 19822 + ((taxableIncome - 87000) * (float)0.37);
-                else if (taxableIncome > 180001)
-                    incomeTax = 54232 + ((taxableIncome - 180000) * (float)0.47);
-                Console.WriteLine("Income Tax: " + incomeTax.ToString("C", CultureInfo.CurrentCulture));
-
-                // calculate Net income
-                float netIncome = grossSalary - superannuation - medicareLevy - budgetRepairLevy - incomeTax;
-                Console.WriteLine("\nNet income: " + netIncome.ToString("C", CultureInfo.CurrentCulture));
-
-                // calculate Pay packet
-                float payPacket = 0;
-                switch (payFreq.ToUpper())
-                {
-                    case "W":
-                        payPacket = (netIncome / 365) * 7;
-                        break;
-                    case "F":
-                        payPacket = (netIncome / 365) * 14;
-                        break;
-                    case "M":
-                        payPacket = netIncome / 12;
-                        break;
-                    default:
-                        Console.Write("An invalid choice was selected for Pay Frequency.\n");
-                        break;
-                }
-                Console.WriteLine("Pay packet: " + payPacket.ToString("C", CultureInfo.CurrentCulture));
+                Console.WriteLine("Pay packet: " + Salary.PayPacket.ToString("C", CultureInfo.CurrentCulture));
             }
             else
             {
-                //bad
                 Console.WriteLine("The entered amount is not a valid salary value.");
             }
 
---
+++ SalaryDetailer/Salary.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryDetailer
+{
+    class Salary
+    {
+        private decimal _grossSalary;
+        public decimal GrossSalary { get { return _grossSalary; } set { _grossSalary = value; } }
+
+        private char _payFrequency;
+        public char PayFrequency { get { return _payFrequency; } set { _payFrequency = value; } }
+
+        private decimal _taxableIncome;
+        public decimal TaxableIncome { get { return _taxableIncome; } set { _taxableIncome = value; } }
+
+        private decimal _superannuation;
+        public decimal Superannuation { get { return _superannuation; } set { _superannuation = value; } }
+
+        private decimal _medicareLevy;
+        public decimal MedicareLevy { get { return _medicareLevy; } set { _medicareLevy = value; } }
+
+        private decimal _budgetRepairLevy;
+        public decimal BudgetRepairLevy { get { return _budgetRepairLevy; } set { _budgetRepairLevy = value; } }
+
+        private decimal _incomeTax;
+        public decimal IncomeTax { get { return _incomeTax; } set { _incomeTax = value; } }
+
+        private decimal _netIncome;
+        public decimal NetIncome { get { return _netIncome; } set { _netIncome = value; } }
+
+        private decimal _payPacket;
+        public decimal PayPacket { get { return _payPacket; } set { _payPacket = value; } }
+
+        public Salary(decimal grossSalary, decimal superannuationPercentage, char payFrequency)
+        {
+            GrossSalary = grossSalary;
+            PayFrequency = payFrequency; 
+            CalculateTaxableIncome(superannuationPercentage);
+            CalculateSuperannuation();
+            CalculateMedicareLevy();
+            CalculateBudgetRepairLevy();
+            CalculateIncomeTax();
+            CalculateNetIncome();
+            CalculatePayPacket();
+        }
+
+        private void CalculateTaxableIncome(decimal superannuationPercentage)
+        {
+            TaxableIncome = GrossSalary / ((100 + superannuationPercentage) / 100);
+        }
+
+        private void CalculateSuperannuation()
+        {
+            Superannuation = GrossSalary - TaxableIncome;
+        }
+
+        private void CalculateMedicareLevy()
+        {
+            decimal medicareLevy = 0;
+            if (TaxableIncome > 21335 && TaxableIncome <= 26668)
+                medicareLevy = (TaxableIncome - 21335) * (decimal)0.1;
+            else if (TaxableIncome > 26669)
+                medicareLevy = TaxableIncome * (decimal)0.02;
+
+            MedicareLevy = medicareLevy;
+        }
+
+        private void CalculateBudgetRepairLevy()
+        {
+            decimal budgetRepairLevy = 0;
+            if (TaxableIncome > 180000)
+                budgetRepairLevy = (TaxableIncome - 180000) * (decimal)0.02;
+
+            BudgetRepairLevy = budgetRepairLevy;
+        }
+
+        private void CalculateIncomeTax()
+        {
+            decimal incomeTax = 0;
+            if (TaxableIncome > 18200 && TaxableIncome <= 37000)
+                incomeTax = (TaxableIncome - 18200) * (decimal)0.19;
+            else if (TaxableIncome > 37000 && TaxableIncome <= 87000)
+                incomeTax = 3572 + ((TaxableIncome - 37000) * (decimal)0.325);
+            else if (TaxableIncome > 87000 && TaxableIncome <= 180000)
+                incomeTax = 19822 + ((TaxableIncome - 87000) * (decimal)0.37);
+            else if (TaxableIncome > 180001)
+                incomeTax = 54232 + ((TaxableIncome - 180000) * (decimal)0.47);
+
+            IncomeTax = incomeTax;
+        }
+
+        private void CalculateNetIncome()
+        {
+            decimal netIncome = GrossSalary - Superannuation - MedicareLevy - BudgetRepairLevy - IncomeTax;
+            NetIncome = netIncome;
+        }
+
+        private void CalculatePayPacket()
+        {
+            decimal payPacket = 0;
+            switch (PayFrequency)
+            {
+                case 'W':
+                    payPacket = (NetIncome / 365) * 7;
+                    break;
+                case 'F':
+                    payPacket = (NetIncome / 365) * 14;
+                    break;
+                case 'M':
+                    payPacket = NetIncome / 12;
+                    break;
+            }
+
+            PayPacket = payPacket;
+        }
+    }
+}
